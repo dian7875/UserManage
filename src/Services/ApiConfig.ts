d92e2419d07ba23290abfeab9011b588405baf35
Obando-26 <166901255+Obando-26@users.noreply.github.com>
2024-11-10 20:50:18 -0600

    variable de entorno

--- src/Services/ApiConfig.ts
+++ src/Services/ApiConfig.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 const api = axios.create({
-  baseURL: "https://localhost:7222/api/",
+  baseURL: baseURL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
