09e48818f10febd64430559282ce315ed45cf833
Dian <dian7875.a@gmail.com>
2024-11-14 15:49:14 -0600

    Codigo duplicado

--- src/Context/UserProvider/UserProvider.tsx
+++ src/Context/UserProvider/UserProvider.tsx
@@ -19,19 +19,7 @@ const UserProvider = ({ children }: { children: React.ReactNode }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const urlParams = new URLSearchParams(window.location.search);
-  const token = urlParams.get("token");
-
   const ClientURL = import.meta.env.VITE_API_CLIENT_URL;
-
-  if (token) {
-    localStorage.setItem("authToken", token);
-    const urlWithoutToken = window.location.href.replace(
-      /([&])token=[^&]+/,
-      ClientURL
-    );
-    window.history.replaceState({}, "", urlWithoutToken);
-  }
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
