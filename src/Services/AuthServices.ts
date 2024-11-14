802d26d25065ec9488f74863dc9126c25b78532e
Dian <dian7875.a@gmail.com>
2024-11-14 15:28:27 -0600

    test

--- src/Services/AuthServices.ts
+++ src/Services/AuthServices.ts
@@ -4,7 +4,7 @@ const GetStats = async () => {
   const response = await api.get("User/statistics/summary");
   return response.data;
 };
-const logginURL = import.meta.env.VITE_API_AUTH_URL;
+
 window.addEventListener("message", (event) => {
 
   console.log("Token:", event.data);
