5ed7175ecbad316d05e4eff0b8570ed09313cbbb
Dian <dian7875.a@gmail.com>
2024-11-13 22:47:54 -0600

    Token por LS

--- src/Services/ApiConfig.ts
+++ src/Services/ApiConfig.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
+const token = localStorage.getItem('Token')
 
 const api = axios.create({
   baseURL: baseURL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
   },
   withCredentials: true, 
 });
